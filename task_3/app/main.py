a9c36609132bebcc5a9e808c1de5513e8a2b45d4
bishalacharya05 <bishalacharya05@gmail.com>
2026-04-01 11:49:23 +0545

    remove pycache

--- task_3/app/main.py
+++ task_3/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.db.base import Base
 from app.db.session import engine
 from app.api.routes import employee_router
+from app.models.employee_model import Employee
 
 app=FastAPI()
 
